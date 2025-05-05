db85e300918dc8f50aaa8b66ef44884d7f41cf1f
Alsaef <saef.ratul@gmail.com>
2025-05-05 14:20:31 +0600

    dev

--- src/Components/HowToWorkTo.jsx
+++ src/Components/HowToWorkTo.jsx
@@ -16,7 +16,7 @@ const HowToWorkTo = () => {
                         <div className="relative  overflow-hidden ">
                             <div
                                 className="group relative cursor-pointer overflow-hidden bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl sm:mx-auto sm:max-w-sm sm:rounded-lg sm:px-10">
-                                <span className="absolute left-10 top-10 z-0 h-20 w-20 rounded-full bg-sky-500 transition-all duration-300 group-hover:scale-[10]"></span>
+                                <span className="absolute lg:left-10 left-6 top-10 z-0 h-20 w-20 rounded-full bg-sky-500 transition-all duration-300 group-hover:scale-[10]"></span>
                                 <div className="relative z-10 mx-auto max-w-md">
                                     <span className="grid h-20 w-20 place-items-center rounded-full bg-sky-500 transition-all duration-300 group-hover:bg-sky-400">
                                         <img src="https://as2.ftcdn.net/jpg/01/58/29/93/1000_F_158299334_geKdmx3GFe7JqiTmfYpkwAJubfLfEpUk.jpg" alt="" />
@@ -24,10 +24,10 @@ const HowToWorkTo = () => {
                                     <div
 
                                         className="space-y-6 pt-5 text-base leading-7 text-gray-600 transition-all duration-300 group-hover:text-white/90">
-                                          <h3 className="text-xl font-semibold">Step 4: Track Your Applications</h3>
-            <p className="text-gray-500 mt-2">
-              Manage your applications and track your job search progress.
-            </p>
+                                        <h3 className="text-xl font-semibold">Step 1: Register or Login</h3>
+                                        <p className="text-gray-500 mt-2">
+                                        Create an account or log in to start exploring job opportunities.
+                                        </p>
                                     </div>
                                     <div className="pt-5 text-base font-semibold leading-7">
                                         <p>
@@ -44,17 +44,17 @@ const HowToWorkTo = () => {
                         <div className="relative  overflow-hidden ">
                             <div
                                 className="group relative cursor-pointer overflow-hidden bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl sm:mx-auto sm:max-w-sm sm:rounded-lg sm:px-10">
-                                <span className="absolute left-10 top-10 z-0 h-20 w-20 rounded-full bg-sky-500 transition-all duration-300 group-hover:scale-[10]"></span>
+                                <span className="absolute lg:left-10 left-6 top-10 z-0 h-20 w-20 rounded-full bg-sky-500 transition-all duration-300 group-hover:scale-[10]"></span>
                                 <div className="relative z-10 mx-auto max-w-md">
                                     <span className="grid h-20 w-20 place-items-center rounded-full bg-sky-500 transition-all duration-300 group-hover:bg-sky-400">
                                         <img src="https://cdn-icons-png.flaticon.com/512/393/393790.png" alt="" />
                                     </span>
                                     <div
                                         className="space-y-6 pt-5 text-base leading-7 text-gray-600 transition-all duration-300 group-hover:text-white/90">
-                                   <h3 className="text-xl font-semibold">Step 4: Track Your Applications</h3>
-            <p className="text-gray-500 mt-2">
-              Manage your applications and track your job search progress.
-            </p>
+                                        <h3 className="text-xl font-semibold">Step 2: Browse Jobs</h3>
+                                        <p className="text-gray-500 mt-2">
+                                        Explore various job listings and learn about the companies hiring.
+                                        </p>
                                     </div>
                                     <div className="pt-5 text-base font-semibold leading-7">
                                         <p>
@@ -72,17 +72,17 @@ const HowToWorkTo = () => {
                         <div className="relative  overflow-hidden ">
                             <div
                                 className="group relative cursor-pointer overflow-hidden bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl sm:mx-auto sm:max-w-sm sm:rounded-lg sm:px-10">
-                                <span className="absolute left-10 top-10 z-0 h-20 w-20 rounded-full bg-sky-500 transition-all duration-300 group-hover:scale-[10]"></span>
+                                <span className="absolute lg:left-10 left-6 top-10 z-0 h-20 w-20 rounded-full bg-sky-500 transition-all duration-300 group-hover:scale-[10]"></span>
                                 <div className="relative z-10 mx-auto max-w-md">
                                     <span className="grid h-20 w-20 place-items-center rounded-full bg-sky-500 transition-all duration-300 group-hover:bg-sky-400">
                                         <img src="https://cdn-icons-png.flaticon.com/512/13441/13441753.png" alt="" />
                                     </span>
                                     <div
                                         className="space-y-6 pt-5 text-base leading-7 text-gray-600 transition-all duration-300 group-hover:text-white/90">
-                                         <h3 className="text-xl font-semibold">Step 4: Track Your Applications</h3>
-            <p className="text-gray-500 mt-2">
-              Manage your applications and track your job search progress.
-            </p>
+                                        <h3 className="text-xl font-semibold">Step 3: Apply for Jobs</h3>
+                                        <p className="text-gray-500 mt-2">
+                                        Apply for your desired jobs directly through the platform.
+                                        </p>
                                     </div>
                                     <div className="pt-5 text-base font-semibold leading-7">
                                         <p>
@@ -99,17 +99,17 @@ const HowToWorkTo = () => {
                         <div className="relative  overflow-hidden ">
                             <div
                                 className="group relative cursor-pointer overflow-hidden bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl sm:mx-auto sm:max-w-sm sm:rounded-lg sm:px-10">
-                                <span className="absolute left-10 top-10 z-0 h-20 w-20 rounded-full bg-sky-500 transition-all duration-300 group-hover:scale-[10]"></span>
+                                <span className="absolute lg:left-10 left-6 top-10 z-0 h-20 w-20 rounded-full bg-sky-500 transition-all duration-300 group-hover:scale-[10]"></span>
                                 <div className="relative z-10 mx-auto max-w-md">
                                     <span className="grid h-20 w-20 place-items-center rounded-full bg-sky-500 transition-all duration-300 group-hover:bg-sky-400">
                                         <img src="https://cdn-icons-png.flaticon.com/512/4012/4012375.png" alt="" />
                                     </span>
                                     <div
                                         className="space-y-6 pt-5 text-base leading-7 text-gray-600 transition-all duration-300 group-hover:text-white/90">
-                                         <h3 className="text-xl font-semibold">Step 4: Track Your Applications</h3>
-            <p className="text-gray-500 mt-2">
-              Manage your applications and track your job search progress.
-            </p>
+                                        <h3 className="text-xl font-semibold">Step 4: Track Your Applications</h3>
+                                        <p className="text-gray-500 mt-2">
+                                        Manage your applications and track your job search progress.
+                                        </p>
                                     </div>
                                     <div className="pt-5 text-base font-semibold leading-7">
                                         <p>
