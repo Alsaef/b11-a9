d75a61e617486ca6258a857f72ee069cf58c3c9e
Alsaef <saef.ratul@gmail.com>
2025-05-07 02:25:18 +0600

    dev

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import DocumentTitle from '../Hook/DocumentTitle';
+import { FaEye, FaRegEyeSlash } from 'react-icons/fa';
 
 const Register = () => {
+    const [showPassword, setShowPassword] = useState(false);
     return (
         <div>
-              <DocumentTitle title='Register | Job Box'></DocumentTitle>
+            <DocumentTitle title='Register | Job Box'></DocumentTitle>
             <div className="hero  min-h-screen">
 
                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
@@ -18,8 +20,11 @@ const Register = () => {
                             <input type="url" className="input" placeholder="PhotoUrl" />
                             <label className="label">Email</label>
                             <input type="email" className="input" placeholder="Email" />
-                            <label className="label">Password</label>
-                            <input type="password" className="input" placeholder="Password" />
+                            <div className='relative'>
+                                <label className="label">Password</label>
+                                <input required type={showPassword === false ? "password" : "text"} name='password' className="input" placeholder="Password" />
+                                <span onClick={() => setShowPassword(!showPassword)} className='absolute top-9 right-7 text-xl transform -translate-y-1/2 z-40 text-gray-600 cursor-pointer '>{showPassword == false ? <FaRegEyeSlash /> : <FaEye />}</span>
+                            </div>
                             <button className="btn bg-blue-400 text-white mt-4">Register</button>
                         </fieldset>
                         <p>You Have Any Account <Link to='/login' className='text-blue-500 font-bold'>Login</Link></p>
